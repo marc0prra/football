5e24c6f98777d7a55f616d9c6a295a6b1c481fb9
SBKF19 <sofiane.benkherouf05@gmail.com>
2025-11-02 16:49:16 +0100

    Creation de la page create_match.php

--- public/create_match.php
+++ public/create_match.php
@@ -10,72 +10,95 @@ $teams = Team::selectTeams();
 $clubs = OpposingClub::selectClubs();
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $infos = DatabaseManager::returnArray($_POST);
-    if (!isset($infos["errors"])) {
-        $match = new Matchs();
-
+    if ((!isset($infos["errors"])) && $infos["score-equipe"] >= 0 && $infos["score-club-oppose"] >= 0) {
+        $match = new Matchs(
+            $infos["score-equipe"],
+            $infos["score-club-oppose"],
+            $infos["date-match"],
+            $infos["équipe"],
+            $infos["city-match"],
+            $infos["club"]
+        );
+        $match->insertMatch();
+        $infos = "";
+        $_SESSION['success'] = "Le match a bien été enregistré !";
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit;
     }
 }
-
 ?>
+
+<?php if (isset($_SESSION['success'])): ?>
+    <div class="success">
+        <?php
+        echo $_SESSION['success'];
+        unset($_SESSION['success']);
+        ?>
+    </div>
+<?php endif; ?>
 
 <form action="" method="post">
     <div>
-        <p class="error"><?php echo isset($infos["errors"]["joueur"]) ? $infos["errors"]["joueur"] : "" ?></p>
-        <label for="date-match">Choisissez une date</label><br>
-        <input type="date" id="date-match" name="date-match" required><br><br>
-
         <div class="row">
+            <div class="column">
+                <p class="error">
+                    <?php echo isset($infos["errors"]["date-match"]) ? $infos["errors"]["date-match"] : "" ?>
+                </p>
+                <label for="date-match">Choisissez une date</label><br>
+                <input type="date" id="date-match" name="date-match" required>
+            </div>
+            <div class="column">
+                <p class="error">
+                    <?php echo isset($infos["errors"]["score-club-oppose"]) ? $infos["errors"]["score-club-oppose"] : "" ?>
+                </p>
+                <label for="select-city">Ville du match</label><br>
+                <input type="text" id="city-match" name="city-match" required>
+            </div>
+        </div>
+        <div class="row">
+
             <div class="column">
                 <p class="error"><?php echo isset($infos["errors"]["équipe"]) ? $infos["errors"]["équipe"] : "" ?></p>
                 <label for="select-team">Choisissez une équipe</label><br>
-                <select name="équipe" id="équipe">
+                <select name="équipe" id="équipe" required>
                     <?php foreach ($teams as $team) { ?>
                         <option value="<?= htmlspecialchars($team->getId()) ?>">
                             <?= htmlspecialchars($team->getName()) ?>
                         </option>
                     <?php } ?>
                 </select>
-            </div><br><br>
+            </div>
+
             <div class="column">
-                <p class="error"><?php echo isset($infos["errors"]["position"]) ? $infos["errors"]["position"] : "" ?>
+                <p class="error">
+                    <?php echo isset($infos["errors"]["score-equipe"]) ? $infos["errors"]["score-equipe"] : "" ?>
                 </p>
-                <label for="select-type">Choisissez une position pour le joueur</label><br>
-                <select name="position" id="position">
-                    <?php foreach ($types as $type) { ?>
-                        <option value="<?= htmlspecialchars($type) ?>">
-                            <?= htmlspecialchars($type) ?>
-                        </option>
-                    <?php } ?>
-                </select>
-            </div><br><br>
+                <label for="select-player">Score équipe</label><br>
+                <input type="number" id="score-equipe" name="score-equipe" required>
+            </div>
+
             <div class="column">
-                <p class="error"><?php echo isset($infos["errors"]["joueur"]) ? $infos["errors"]["joueur"] : "" ?></p>
-                <label for="select-player">Choisissez un joueur</label><br>
-                <input type="" id="date-match" name="date-match" required><br><br>
-            </div><br><br>
+                <p class="error">
+                    <?php echo isset($infos["errors"]["score-club-oppose"]) ? $infos["errors"]["score-club-oppose"] : "" ?>
+                </p>
+                <label for="select-player">Score club opposé</label><br>
+                <input type="number" id="score-club-oppose" name="score-club-oppose" required>
+            </div>
+
             <div class="column">
-                <p class="error"><?php echo isset($infos["errors"]["équipe"]) ? $infos["errors"]["équipe"] : "" ?></p>
+                <p class="error"><?php echo isset($infos["errors"]["club"]) ? $infos["errors"]["club"] : "" ?></p>
                 <label for="select-team">Choisissez un club</label><br>
-                <select name="équipe" id="équipe">
-                    <?php foreach ($teams as $team) { ?>
-                        <option value="<?= htmlspecialchars($team->getId()) ?>">
-                            <?= htmlspecialchars($team->getName()) ?>
+                <select name="club" id="club" required>
+                    <?php foreach ($clubs as $club) { ?>
+                        <option value="<?= htmlspecialchars($club->getId()) ?>">
+                            <?= htmlspecialchars($club->getCity()) ?>
                         </option>
                     <?php } ?>
                 </select>
-            </div><br><br>
+            </div>
+
         </div>
-        <div class="column"></div>
-        <p class="error"><?php echo isset($infos["errors"]["position"]) ? $infos["errors"]["position"] : "" ?></p>
-        <label for="select-type">Choisissez une position pour le joueur</label><br>
-        <select name="position" id="position">
-            <?php foreach ($types as $type) { ?>
-                <option value="<?= htmlspecialchars($type) ?>">
-                    <?= htmlspecialchars($type) ?>
-                </option>
-            <?php } ?>
-        </select>
-    </div><br><br>
+    </div>
     </div>
 
     <button type="submit">Ajouter à l'équipe</button>
